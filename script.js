40d3134d24ccbad40e51d07696b3b593c94e04e1
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-03-02 18:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const textElement = docuement.querySelector("#evaluatedText");
+const h3Element = docuement.querySelector("#wordCount");
+
+function handleInput(e){
+	const sentence = e.target.value;
+	const count = sentence.match(/\S+/ig).length :0;
+	h3Element.textContent = count;
+}
+	textElement.addEventListener('input', handleInput);
+	
